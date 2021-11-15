6e1a57d43eb88c8985c24848aa208f90e270b2f7
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-11-15 13:34:33 +0530

    swapping target and independent data

--- index.py
+++ index.py
@@ -277,13 +277,13 @@ if step == 3:
         fig, (ax1, ax2) = plt.subplots(1, 2)
         fig.set_figheight(6)
         fig.set_figwidth(15)
-        ax1.plot(log(globals()[list(var_dict.keys())[0]]), log(pred.real), color='r')
-        ax1.plot(log(globals()[list(var_dict.keys())[0]]), log(Real), color='k', marker="*",
+        ax1.plot(log(globals()[list(var_dict.keys())[0]]), log(Real), color='r')
+        ax1.plot(log(globals()[list(var_dict.keys())[0]]), log(pred.real), color='k', marker="*",
                 linestyle='', markersize=10)
         ax1.set_title("Real", fontsize=12)
 
-        ax2.plot(log(globals()[list(var_dict.keys())[0]]), log(pred.imag), color='b')
-        ax2.plot(log(globals()[list(var_dict.keys())[0]]), log(Img), color='c', marker="*",
+        ax2.plot(log(globals()[list(var_dict.keys())[0]]), log(Img), color='b')
+        ax2.plot(log(globals()[list(var_dict.keys())[0]]), log(pred.imag), color='c', marker="*",
                 linestyle='', markersize=10)
         ax2.set_title("Imaginary", fontsize=12)
         plt = fig
